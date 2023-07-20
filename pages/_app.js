9424f10a56a7317a86766b5a83f8111c0b9316c3
Vladimir <bigcorvet@gmail.com>
2023-07-20 11:47:14 +0300

    added _app.js

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+//import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App({ Component, pageProps }) {
+    return (
+        <Component {...pageProps} />
+    )
+}
