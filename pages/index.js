b4ea93d1fc2799b3cdfddc656e7d40982b234f3a
Vladimir <bigcorvet@gmail.com>
2023-07-20 15:59:42 +0300

    added GeneralText Component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from "next/head"
-import Layout from "../components/layout/layout"
+import Layout from "../components/layout/Layout"
 import Slider from "../components/slider/Slider"
 import Categories from "../components/categories/categories"
 import Brands from "../components/brands/brands"
 import Promo from "../components/promo/Promo"
 import WatchList from "../components/watchList/WatchList"
-import { siteTitle } from "../components/layout/layout"
+import GeneralText from "../components/generaltext/GeneralText"
+import { siteTitle } from "../components/layout/Layout"
 
 export default function Home() {
     return (
@@ -18,6 +19,7 @@ export default function Home() {
             <Brands />
             <Promo />
             <WatchList />
+            <GeneralText />
         </Layout>
     )
 }
