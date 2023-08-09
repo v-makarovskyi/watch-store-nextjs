0a4f20d6158246530206ab536f968f135d5af653
Vladimir <bigcorvet@gmail.com>
2023-08-09 11:58:21 +0300

    added watch-list.module.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
+import "../styles/global.css";
 //import "bootstrap/dist/css/bootstrap.min.css";
+import { wrapper } from "../redux/store";
+import { Provider } from "react-redux";
+import Layout from "../components/layout/Layout";
 
-export default function App({ Component, pageProps }) {
-    return (
-        <Component {...pageProps} />
-    )
+export default function App({ Component, ...rest }) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store}>
+        <Component {...props.pageProps} />
+    </Provider>
+  );
 }
+
