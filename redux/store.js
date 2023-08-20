63cc9abf91833685b1c53d5b1753b32b6f447b4c
Vladimir <bigcorvet@gmail.com>
2023-08-20 21:02:21 +0300

    added store rtk

---
+++ redux/store.js
@@ -0,0 +1,48 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { createWrapper } from "next-redux-wrapper";
+import { watchsApi } from "./watchsApi";
+/* import productsReducer from "./products/productReducer";
+import categoriesReducer from "./categories/categoriesReduser";
+import brandsReducer from "./brands/brandsSlice"; */
+
+
+export const makeStore = () => 
+  configureStore({
+    reducer: {
+      [watchsApi.reducerPath]: watchsApi.reducer
+    },
+    middleware: (gDM) => gDM().concat(watchsApi.middleware)
+  })
+
+export const wrapper = createWrapper(makeStore, {debug:true})
+
+
+
+
+/* const allReducers = {
+  products: productsReducer,
+  categories: categoriesReducer,
+  brands: brandsReducer,
+}
+
+const combinedReducer = combineReducers(allReducers);
+
+const reducer = (state, action) => {
+  if (action.type === 'HYDRATE') {
+    const nextState = {
+      ...state,
+      ...action.payload,
+    };
+    return nextState;
+  } else {
+    return combinedReducer(state, action);
+  }
+};
+
+export const makeStore = () => {
+  return configureStore({
+    reducer,
+  });
+};
+
+export const wrapper = createWrapper(makeStore); */
