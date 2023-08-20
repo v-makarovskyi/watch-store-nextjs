4a347d70dc6a4ded23cb3b825fbe4b18110e1ead
Vladimir <bigcorvet@gmail.com>
2023-08-20 20:59:12 +0300

    added file Filtered Data

---
+++ data/filterData.js
@@ -0,0 +1,56 @@
+export const watchtypeFilterData = [
+  {
+    id: 0,
+    keyword: "watch_type",
+    title: "Тип годинника",
+    variants: [
+      { name: "Чоловічий", value: "m" },
+      { name: "Жіночий", value: "w" },
+    ],
+  },
+]
+
+export const brandFilterData = [
+  {
+    id: 1,
+    keyword: "brand",
+    title: "Бренд",
+    variants: [
+      { name: "ATLANTIC", value: "atlantic" },
+      { name: "TISSOT", value: "tissot" },
+      { name: "LONGINES", value: "longines" },
+      { name: "HAMILTON", value: "hamilton" },
+      { name: "CASIO", value: "casio" },
+      { name: "SEIKO", value: "seiko" },
+    ],
+  },
+]
+
+export const glassFilterData = [
+  {
+    id: 2,
+    keyword: "glass",
+    title: "Тип скла",
+    variants: [
+      { name: "Сапфірове", value: "sapf" },
+      { name: "Мінеральне", value: "min" },
+    ],
+  },
+]
+  
+
+  
+export const wristbandFilterData = [
+  {
+    id: 3,
+    keyword: "wristband",
+    title: "Браслет",
+    variants: [
+      { name: "Стальний", value: "steel" },
+      { name: "Шкіряний", value: "leather" },
+      { name: "Каучуковий", value: "rubber" },
+    ],
+  },
+]
+  
+  
