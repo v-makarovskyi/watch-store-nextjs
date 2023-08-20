8069954f442bcaa3ee0c5f81740763430b8dd3d0
Vladimir <bigcorvet@gmail.com>
2023-08-20 20:56:57 +0300

    added function view for component

--- components/productSlider/ProductSlider.jsx
+++ components/productSlider/ProductSlider.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ImageGallery from "react-image-gallery";
 
-
 export default function ProductSlider({ images }) {
-  const imgs = images.map((img) => {
+ 
+  const imgs = images?.map((img) => {
     const item = {
-       original: img.image,
-       thumbnail: img.image
-    }
-    return item
-  }) 
-  
+      original: img.image,
+      thumbnail: img.image,
+    };
+    return item;
+  });
 
   return (
     <ImageGallery
