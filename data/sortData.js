9a0ce80f28df137497ab71ab7bc4810502c0973b
Vladimir <bigcorvet@gmail.com>
2023-08-21 16:52:16 +0300

    added sort data

---
+++ data/sortData.js
@@ -0,0 +1,6 @@
+export const sortData = [
+    {id:'0', title: 'Спочатку новіші', value:'new'},
+    {id:'1', title: 'Сортувати по вартості: спочатку дешевші', value:'asc'},
+    {id:'2', title: 'Сортувати по вартості: спочатку дорожчі', value:'desc'},
+    {id:'3', title: 'Хіти продажів', value:'hit'},
+]
