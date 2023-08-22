b2999770e2c5227b09be250c21e30860fa2c8fa1
Vladimir <bigcorvet@gmail.com>
2023-08-22 16:21:08 +0300

    added ErrorMessage component

--- components/common/errorMsg/ErrorMsg.jsx
+++ components/common/errorMsg/ErrorMsg.jsx
@@ -1,6 +1,25 @@
 import React from "react";
-import './errormsg.module.css'
+import { useRouter } from "next/router";
+import styles from "./errormsg.module.css";
 
 export default function ErrorMsg({ message }) {
-  return <div style={{ color: "red" }}>{message}</div>;
+  const router = useRouter()
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.message}>{message}</p>
+      <div className={styles.btns}>
+        <button className={`${styles.error_btn} ${styles.error_btn_prev}`}
+          onClick={() => router.back()}
+        >
+          <i className="bi bi-arrow-return-left"></i> <span>На предыдущую</span>
+        </button>
+        <button className={`${styles.error_btn} ${styles.error_btn_main}`}
+          onClick={() => router.push('/')}
+        >
+          <span>На главную</span> <i className="bi bi-arrow-return-right"></i>
+        </button>
+      </div>
+    </div>
+  );
 }
