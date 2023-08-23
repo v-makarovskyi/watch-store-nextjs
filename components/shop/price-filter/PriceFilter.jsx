1b71c57ff3d81276a95f2f737049eb02f7a3cb59
Vladimir <bigcorvet@gmail.com>
2023-08-23 13:06:00 +0300

    added Loader

--- components/shop/price-filter/PriceFilter.jsx
+++ components/shop/price-filter/PriceFilter.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import InputRange from "./InputRange";
+import PriceFilterLoader from "../../loader/shop/PriceFilterLoader";
+import { useRouter } from "next/router";
 import styles from "./price-filter.module.css";
 
 export default function PriceFilter({ priceValue, handleChanges, watchs }) {
+  const router = useRouter();
+
   const maxPrice = watchs?.reduce((acc, watch) => {
     return watch?.price > acc ? watch.price : acc;
   }, 0);
 
-  return (
-    <div className={styles.container}>
+  const { isLoading } = router;
+  let content = null;
+  content = <PriceFilterLoader loading={isLoading} width='50' />;
+ if (isLoading) {
+    content = <PriceFilterLoader loading={isLoading} />;
+  } else {
+    content = (
       <div className={styles.wrapper}>
         <h4 className={styles.title}>Ціна</h4>
         <div className={styles.slider_range}>
@@ -25,8 +34,13 @@ export default function PriceFilter({ priceValue, handleChanges, watchs }) {
           {priceValue[0]}грн - {priceValue[1]}грн
         </span>
       </div>
+    );
+  } 
 
-      <hr />
+  return (
+    <div className={styles.container}>
+      {content}
+    <hr />
     </div>
   );
 }
