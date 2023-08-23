282f98f969aee45d13e81078e4c655c63fff1f65
Vladimir <bigcorvet@gmail.com>
2023-08-23 13:06:24 +0300

    added new field

--- data/sortData.js
+++ data/sortData.js
@@ -2,5 +2,5 @@ export const sortData = [
     {id:'0', title: 'Спочатку новіші', value:'new'},
     {id:'1', title: 'Сортувати по вартості: спочатку дешевші', value:'asc'},
     {id:'2', title: 'Сортувати по вартості: спочатку дорожчі', value:'desc'},
-    {id:'3', title: 'Хіти продажів', value:'hit'},
+    {id:'3', title: 'Годинники зі знижкою', value:'discount'},
 ]
