d0682e431d2a19c3cb05dfb3dad285020b85f362
Vladimir <bigcorvet@gmail.com>
2023-08-24 18:13:50 +0300

    added component

---
+++ components/loader/shop/WristbandFilterLoader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Loader from "../Loader";
+
+function SingleLoader({ loading, width }) {
+  return (
+    <div
+      aria-label="price-filter-loader"
+      style={{
+        height: `${width}px`,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Loader loading={loading} spinner="propagate" />
+    </div>
+  );
+}
+
+export default function WristbandFilterLoader({ loading, width }) {
+  return <SingleLoader loading={loading} width={width} />;
+}
