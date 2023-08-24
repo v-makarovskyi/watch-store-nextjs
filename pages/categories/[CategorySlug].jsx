2d65e890507e127e95142c5ce0ead957eb39c33d
Vladimir <bigcorvet@gmail.com>
2023-08-24 18:12:20 +0300

    added Sort andFilters on page

--- pages/categories/[CategorySlug].jsx
+++ pages/categories/[CategorySlug].jsx
@@ -1,238 +1,232 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { useSelector, dispatch, useDispatch } from "react-redux";
-import axiosElement from "../../utils/axios-element";
-import DOMPurify from 'isomorphic-dompurify';
+import { useSelector } from "react-redux";
+import DOMPurify from "isomorphic-dompurify";
 import Layout from "../../components/layout/Layout";
 import { Row, Col, Breadcrumb } from "react-bootstrap";
 import WatchList from "../../components/watchList/WatchList";
+import FilterProperties from "../../components/shop/shop-filters/FilterProperties";
 import { wrapper } from "../../redux/store";
-import { fetchSingleCategory, fetchWatchsFromRelatedCategory } from "../../redux/categories/categoriesReduser";
+import Sort from "../../components/shop/sort-block/Sort";
+import ErrorMsg from "../../components/common/errorMsg/ErrorMsg";
 import styles from "./category.module.css";
-import { categoryDetails } from "../../redux/categories/categoriesReduser";
+import {
+  getCategory,
+  getRunningQueriesThunk,
+  useGetCategoryQuery,
+  getCategoryListWatchs,
+  useGetCategoryListWatchsQuery,
+  useGetAllWatchsQuery,
+} from "../../redux/watchsApi";
 
 export const getServerSideProps = wrapper.getServerSideProps(
-  (store) => 
-    async({ query }) => {
-      const CategorySlug = query.CategorySlug
-      console.log(CategorySlug)
-      await store.dispatch(fetchSingleCategory(CategorySlug))
-      await store.dispatch(fetchWatchsFromRelatedCategory(CategorySlug)) 
-      const { categories } = store.getState()
+  (store) => async (context) => {
+    const categorySlug = context.query.categorySlug;
+    store.dispatch(getCategory.initiate(categorySlug));
+    store.dispatch(
+      getCategoryListWatchs.initiate({ slug: categorySlug, queryParams: {} })
+    );
 
-      return {
-        props: {
-          category: categories.server.category,
-          products: categories.server.productsForCategory,
-          
-        }
-      }
-    }
-)
+    await Promise.all(store.dispatch(getRunningQueriesThunk()));
 
-export default function Category({category, products}) {
+    return {
+      props: {
+        query: context.query,
+      },
+    };
+  }
+);
+
+export default function Category({ query }) {
+  const categorySlug = query.categorySlug;
 
   const [showSortMenu, setShowSortMenu] = useState(false);
   const [showProperties, setShowProperties] = useState(true);
-  const categoryDescriptionData = category.description
-  
+  const [priceValue, setPriceValue] = useState([0, 0]);
+
+  const {
+    data: category,
+    isError,
+    isLoading,
+    isFetching,
+  } = useGetCategoryQuery(categorySlug);
+
+  const queryParams = {};
+  if (query.watch_type) {
+    queryParams["watch_type"] = query.watch_type;
+  }
+  if (query.glass) {
+    queryParams["glass"] = query.glass;
+  }
+  if (query.wristband) {
+    queryParams["wristband"] = query.wristband;
+  }
+  if (query.brand) {
+    queryParams["brand"] = query.brand;
+  }
+  if (query.color) {
+    queryParams["color"] = query.color;
+  }
+
+  const { data: watchs } = useGetCategoryListWatchsQuery({
+    slug: categorySlug,
+    queryParams,
+  });
+  const { data: allWatchs } = useGetAllWatchsQuery(queryParams);
+
+  const activeSortType = useSelector((state) => state.sort.sortBy.type);
+
+  useEffect(() => {
+    if (category?.name === "Наручні годинники") {
+      if (!isLoading && !isError && allWatchs?.length > 0) {
+        const maxPrice = allWatchs.reduce((acc, watch) => {
+          return watch.price > acc ? watch.price : acc;
+        }, 0);
+        setPriceValue([0, maxPrice]);
+      }
+    } else {
+      if (!isLoading && !isError && watchs?.length > 0) {
+        const maxPrice = watchs.reduce((acc, watch) => {
+          return watch.price > acc ? watch.price : acc;
+        }, 0);
+        setPriceValue([0, maxPrice]);
+      }
+    }
+  }, [isLoading, isError, watchs, allWatchs]);
+
+  function handleChanges(val) {
+    /*  setCurrentPage(1); */
+    setPriceValue(val);
+  }
+
+  let content = null;
+
+  if (category?.name === "Наручні годинники") {
+    if (!isLoading && isError) {
+      content = <ErrorMsg message="Произошла ошибка! Попробуйте еще раз!" />;
+    }
+    if (!isLoading && !isError && allWatchs?.length === 0) {
+      content = <ErrorMsg message="Товары по данному запросу не найдены!" />;
+    }
+  } else {
+    if (!isLoading && isError) {
+      content = <ErrorMsg message="Произошла ошибка! Попробуйте еще раз!" />;
+    }
+    if (!isLoading && !isError && watchs?.length === 0) {
+      content = <ErrorMsg message="Товары по данному запросу не найдены!" />;
+    }
+  }
+
+  if (category?.name === "Наручні годинники") {
+    if (!isLoading && !isError && allWatchs?.length > 0) {
+      let watch_items = allWatchs;
+      if (activeSortType) {
+        if (activeSortType === "new") {
+          const newAllWatchItems = [...watch_items];
+          watch_items = newAllWatchItems.sort(
+            (a, b) => new Date(b.created_at) - new Date(a.created_at)
+          );
+        }
+        if (activeSortType === "asc") {
+          const newAllWatchItems = [...watch_items];
+          watch_items = newAllWatchItems.sort((a, b) => a.price - b.price);
+        }
+        if (activeSortType === "desc") {
+          watch_items.slice().sort((a, b) => b.price - a.price);
+        }
+        if (activeSortType === "discount") {
+          watch_items = watch_items.filter((w) => w.discount > 0);
+        }
+      }
+      //price filter
+      watch_items = watch_items.filter(
+        (w) => w.price >= priceValue[0] && w.price <= priceValue[1]
+      );
+
+      content = <WatchList watchs={watch_items} />;
+    }
+  } else {
+    if (!isLoading && !isError && watchs?.length > 0) {
+      let watch_items = watchs;
+      if (activeSortType) {
+        if (activeSortType === "new") {
+          const newWatchItems = [...watch_items];
+          watch_items = newWatchItems.sort(
+            (a, b) => new Date(b.created_at) - new Date(a.created_at)
+          );
+        }
+        if (activeSortType === "asc") {
+          const newWatchItems = [...watch_items];
+          watch_items = newWatchItems.sort((a, b) => a.price - b.price);
+        }
+        if (activeSortType === "desc") {
+          watch_items.slice().sort((a, b) => b.price - a.price);
+        }
+        if (activeSortType === "discount") {
+          watch_items = watch_items.filter((w) => w.discount > 0);
+        }
+      }
+      //price filter
+      watch_items = watch_items.filter(
+        (w) => w.price >= priceValue[0] && w.price <= priceValue[1]
+      );
+
+      content = <WatchList watchs={watch_items} />;
+    }
+  }
+
   return (
     <Layout>
       <Head>
-        <title>{category.name}</title>
+        <title>{category?.name}</title>
       </Head>
       <Row className={styles.category_container}>
         <Col lg={3} className={styles.right}>
+          {isLoading && <p>LOADING...</p>}
+          {isFetching && <p>FETCHING...</p>}
           <Breadcrumb
             style={{ fontSize: "0.8rem", color: "#bab6b6", paddingTop: "2rem" }}
           >
             <Breadcrumb.Item>Головна</Breadcrumb.Item>
-            <Breadcrumb.Item>{category.name}</Breadcrumb.Item>
+            <Breadcrumb.Item>{category?.name}</Breadcrumb.Item>
           </Breadcrumb>
-          {/*  man or woman type */}
           <hr />
-          <div className={styles.category_container_properties}>
-            <div className={styles.category_container_properties_top}>
-              <h4>Тип годинника</h4>
-            </div>
-            <div
-              className={
-                showProperties
-                  ? styles.category_container_variants
-                  : styles.category_container_variants_hidden
-              }
-            >
-              <label htmlFor="mantype">
-                <input id="mantype" type="checkbox" value="mantype" />
-                Чоловічий годинник
-              </label>
-              <label htmlFor="womantype">
-                <input id="womantypetype" type="checkbox" value="womantype" />
-                Жіночий годинник
-              </label>
-            </div>
-          </div>
-
-          <hr />
-          {/*   brand */}
-
-          <div className={styles.category_container_properties}>
-            <div className={styles.category_container_properties_top}>
-              <h4>Бренд</h4>
-            </div>
-            <div
-              className={
-                showProperties
-                  ? styles.category_container_variants
-                  : styles.category_container_variants_hidden
-              }
-            >
-              <label htmlFor="TISSOT">
-                <input id="TISSOT" type="checkbox" value="TISSOT" />
-                TISSOT
-              </label>
-              <label htmlFor="ATLANTIC">
-                <input id="ATLANTIC" type="checkbox" value="ATLANTIC" />
-                ATLANTIC
-              </label>
-              <label htmlFor="CASIO">
-                <input id="CASIO" type="checkbox" value="CASIO" />
-                CASIO
-              </label>
-              <label htmlFor="HAMILTON">
-                <input id="HAMILTON" type="checkbox" value="HAMILTON" />
-                HAMILTON
-              </label>
-              <label htmlFor="SEIKO">
-                <input id="SEIKO" type="checkbox" value="SEIKO" />
-                SEIKO
-              </label>
-              <label htmlFor="LONGINES">
-                <input id="LONGINES" type="checkbox" value="LONGINES" />
-                LONGINES
-              </label>
-            </div>
-          </div>
-
-          <hr />
-
-          {/* glass */}
-
-          <div className={styles.category_container_properties}>
-            <div className={styles.category_container_properties_top}>
-              <h4>Скло</h4>
-            </div>
-            <div
-              className={
-                showProperties
-                  ? styles.category_container_variants
-                  : styles.category_container_variants_hidden
-              }
-            >
-              <label htmlFor="sapfir">
-                <input id="sapfir" type="checkbox" value="sapfir" />
-                Сапфірове
-              </label>
-              <label htmlFor="mineral">
-                <input id="mineral" type="checkbox" value="mineral" />
-                Мінеральне
-              </label>
-            </div>
-          </div>
-
-          <hr />
-
-          {/*  wristband */}
-
-          <div className={styles.category_container_properties}>
-            <div className={styles.category_container_properties_top}>
-              <h4>Браслет</h4>
-              {/*    {
-                <button onClick={() => setShowProperties(!showProperties)}>
-                  {showProperties ? (
-                    <i class="bi bi-arrow-up-circle"></i>
-                  ) : (
-                    <i class="bi bi-arrow-down-circle"></i>
-                  )}
-                </button>
-              } */}
-            </div>
-            <div
-              className={
-                showProperties
-                  ? styles.category_container_variants
-                  : styles.category_container_variants_hidden
-              }
-            >
-              <label htmlFor="steel">
-                <input id="steel" type="checkbox" value="steel" />
-                Нержавіюча сталь
-              </label>
-              <label htmlFor="leather">
-                <input id="leather" type="checkbox" value="leather" />
-                Шкіряний (М'який, комфортний)
-              </label>
-              <label htmlFor="rubber">
-                <input id="rubber" type="checkbox" value="rubber" />
-                Каучуковий
-              </label>
-            </div>
-          </div>
-
-          <hr />
+          <FilterProperties
+            showProperties={showProperties}
+            categoryPage
+            onHandleChanges={handleChanges}
+            priceValue={priceValue}
+            watchs={category?.name === "Наручні годинники" ? allWatchs : watchs}
+          />
         </Col>
         <Col lg={9} className={styles.left}>
-          <div
-            className={styles.category_sort}
-            onClick={() => setShowSortMenu(!showSortMenu)}
-          >
-            <span>
-              <i class="bi bi-filter-left"></i>
-            </span>
-          </div>
-          <div
-            className={
-              showSortMenu
-                ? styles.category_sort_dropdown
-                : styles.category_sort_dropdown_hidden
-            }
-          >
-            <ul className={styles.category_sort_dropdown_list}>
-              <li className={styles.category_sort_dropdown_list_item}>
-                <span>Спочатку новіші</span>
-              </li>
-              <li className={styles.category_sort_dropdown_list_item}>
-                <span>Сортувати по вартості: спочатку дешевші</span>
-              </li>
-              <li className={styles.category_sort_dropdown_list_item}>
-                <span>Сортувати по вартості: спочатку дорожчі</span>
-              </li>
-            </ul>
-          </div>
+          <Sort
+            showSortMenu={showSortMenu}
+            setShowSortMenu={setShowSortMenu}
+            activeSortType={activeSortType}
+          />
           <h2 className={styles.category_title}>
-            Брендові годинники {category.name}
+            Брендові годинники {category?.name}
           </h2>
-          <WatchList watchs={products}/>
+
+          {content}
+
           <div className={styles.category_description}>
-            {/* <div className={styles.category_description_img_container}>
-              <img src={category.brand_image} />
-            </div> */}
             <div className={styles.category_description_content}>
-             {/*  <p className={styles.category_description_conten_title}>
-                Ми офіційний продавець марки {category.name} в Україні, з
-                обслуговуванням клієнтів тільки в офіційних сервісних центрах.
-              </p> */}
               <div className={styles.category_description_content_body}>
-               <p className={styles.category_description_content_body_img}>
-                  <img src={category.description_image}/>
-                </p> 
-                <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(categoryDescriptionData)}}></div>
+                <p className={styles.category_description_content_body_img}>
+                  <img src={category?.description_image} />
+                </p>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(category?.description),
+                  }}
+                ></div>
               </div>
             </div>
           </div>
         </Col>
       </Row>
     </Layout>
-    );
+  );
 }
