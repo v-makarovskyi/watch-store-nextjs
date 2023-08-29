32e0b4d73318cda24a32f3c529d4ce29863ff147
Vladimir <bigcorvet@gmail.com>
2023-08-29 20:10:32 +0300

    added search_fields & filter_backends

--- watch-store-api/watch_store/views.py
+++ watch-store-api/watch_store/views.py
@@ -5,9 +5,10 @@ from rest_framework.generics import GenericAPIView
 from .models import Watch, Category, Brand
 from .serializers import WatchSerializer, CategorySerializer, BrandSerializer
 from .filters import WatchFilter
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from django.shortcuts import render, get_object_or_404
-
 
 
 class WatchListView(generics.ListAPIView):
@@ -15,7 +16,9 @@ class WatchListView(generics.ListAPIView):
         is_active=True).prefetch_related('watch_image')
     serializer_class = WatchSerializer
     filterset_class = WatchFilter
-    
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['^model', '^watch_type', '^glass',
+                     '^title', '^appearance_type', '^wristband', '^brand__title']
 
 
 class WatchView(generics.RetrieveAPIView):
@@ -23,28 +26,29 @@ class WatchView(generics.RetrieveAPIView):
     queryset = Watch.objects.filter(is_active=True)
     serializer_class = WatchSerializer
 
+
 class Categories(GenericAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.filter(is_active=True)
 
     def get(self, request, *args, **kwargs):
         root_nodes = self.get_queryset().get_cached_trees()
-        data = [self.recursive_node_to_dict(n) for n in root_nodes ]
+        data = [self.recursive_node_to_dict(n) for n in root_nodes]
         return Response(data)
 
     def recursive_node_to_dict(self, node):
         result = self.get_serializer(instance=node).data
-        children = [self.recursive_node_to_dict(c) for c in node.get_children()]
+        children = [self.recursive_node_to_dict(
+            c) for c in node.get_children()]
         if children:
             result['children'] = children
         return result
 
 
 class CategorySingle(generics.RetrieveAPIView):
-    lookup_field='slug'
+    lookup_field = 'slug'
     queryset = Category.objects.filter(is_active=True)
     serializer_class = CategorySerializer
-  
 
 
 class CategoryItemProductsListView(generics.ListAPIView):
@@ -54,15 +58,18 @@ class CategoryItemProductsListView(generics.ListAPIView):
     def get_queryset(self):
         self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
         return Watch.objects.filter(category=self.category)
-    
+
+
 class BrandsAllView(generics.ListAPIView):
     serializer_class = BrandSerializer
     queryset = Brand.objects.filter(is_active=True)
+
 
 class BrandSingleView(generics.RetrieveAPIView):
     lookup_field = 'slug'
     queryset = Brand.objects.filter(is_active=True)
     serializer_class = BrandSerializer
+
 
 class BrandItemProductListView(generics.ListAPIView):
     serializer_class = WatchSerializer
