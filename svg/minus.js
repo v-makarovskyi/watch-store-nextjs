da25c132e1f8bea3320f046ca1ee12367bbeeb6f
Vladimir <bigcorvet@gmail.com>
2023-09-08 20:40:10 +0300

    added components from svg

---
+++ svg/minus.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Minus() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="red"
+      className="bi bi-dash"
+      viewBox="0 0 16 16"
+    >
+      <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+    </svg>
+  );
+}
