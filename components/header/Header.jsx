d5d008ccc5863b27548ed3e13bbef7dae2611402
Vladimir <bigcorvet@gmail.com>
2023-09-08 20:31:34 +0300

    added useEffect

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Container } from "react-bootstrap";
 import { clearSortBy } from "../../redux/sortSlice";
 import HeaderTop from "./header-top/HeaderTop";
 import HeaderMiddle from "./header-middle/HeaderMiddle";
 import HeaderBottom from "./header-bottom/HeaderBottom";
-import styles from "./header.module.css";
+import styles from "./header.module.scss";
 import Link from "next/link";
 import {
   useGetCategoriesQuery,
@@ -13,7 +14,6 @@ import {
 import axiosElement from "../../utils/axios-element";
 import HeaderSearchForm from "../forms/header-search-form/HeaderSearchForm";
 
-
 const languages = [
   { id: 1, flag: "🇺🇦", title: "Українська" },
   { id: 2, flag: "🇬🇧", title: "English" },
@@ -21,8 +21,6 @@ const languages = [
 ];
 
 export default function Header() {
-  
-
   const { data: categories } = useGetCategoriesQuery();
   const { data: brands } = useGetBrandsQuery();
 
@@ -35,6 +33,15 @@ export default function Header() {
   const [showModal, setShowModal] = useState(false);
   const [activeCategory, setActiveCategory] = useState({});
   const [showBrands, setShowBrands] = useState(false);
+  const [width, setWidth] = useState(1440);
+
+  useEffect(() => {
+    const handleClientWidth = () => {
+      setWidth(document.body.clientWidth);
+    };
+    window.addEventListener("resize", handleClientWidth);
+    return () => window.removeEventListener("resize", handleClientWidth);
+  }, []);
 
   function handleSelectLanguage(itemId) {
     const nextCurrentLanguage = languages.find((lang) => lang.id === itemId);
@@ -50,30 +57,26 @@ export default function Header() {
   }
 
   return (
-    <header className={styles.header}>
-      <HeaderTop 
-         languages={languages}
-         onHandleSelectLanguage={handleSelectLanguage}
-         setShowLangugesList={setShowLangugesList}
-         showLanguagesList={showLanguagesList}
-         currentLanguage={currentLanguage}
-      />
-      
-  
-     <HeaderMiddle 
-      setShowModal={setShowModal}
-      showModal={showModal}
-     />
-
-      {/*  Header Bottom */}
-      <HeaderBottom 
-        categories={categories}
-        brands={brands}
-        activeCategory={activeCategory}
-        onHandleActiveCategory={handleActiveCategory}
-        showBrands={showBrands}
-        setShowBrands={setShowBrands}
-      />
-    </header>
+    <div className={styles.header}>
+      <Container fluid className={styles.container}>
+        <HeaderTop
+          languages={languages}
+          onHandleSelectLanguage={handleSelectLanguage}
+          setShowLangugesList={setShowLangugesList}
+          showLanguagesList={showLanguagesList}
+          currentLanguage={currentLanguage}
+        />
+        <HeaderMiddle setShowModal={setShowModal} showModal={showModal} width={width} />
+        <HeaderBottom
+          width={width}
+          categories={categories}
+          brands={brands}
+          activeCategory={activeCategory}
+          onHandleActiveCategory={handleActiveCategory}
+          showBrands={showBrands}
+          setShowBrands={setShowBrands}
+        />
+      </Container>
+    </div>
   );
 }
