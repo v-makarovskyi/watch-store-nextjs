8dc258799ff4c4f47e1ec14843c817b434eade16
Vladimir <bigcorvet@gmail.com>
2023-09-08 20:30:14 +0300

    added sassOptions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 module.exports = {
+    reactStrictMode: true,
     images: {
         domains: ['127.0.0.1']
     },
+    i18n: {
+        locales: ['en-Us', 'ru', 'ua'],
+        defaultLocale: 'ru',
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')]
+    }
 }
