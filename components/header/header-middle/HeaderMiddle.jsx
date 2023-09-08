852cc8e4f2c03b948e180170de958fb23a6ee65d
Vladimir <bigcorvet@gmail.com>
2023-09-08 20:34:58 +0300

    added Search

--- components/header/header-middle/HeaderMiddle.jsx
+++ components/header/header-middle/HeaderMiddle.jsx
@@ -1,67 +1,69 @@
-import React from 'react'
-import Link from 'next/link'
-import HeaderSearchForm from '../../forms/header-search-form/HeaderSearchForm'
-import HeaderModal from './HeaderModal'
-import styles from '../header.module.css'
+import React from "react";
+import Link from "next/link";
+import { Row, Col } from "react-bootstrap";
+import HeaderSearchForm from "../../forms/header-search-form/HeaderSearchForm";
+import { useSelector } from "react-redux";
+import HeaderModal from "./HeaderModal";
+import Heart from "../../../svg/heart";
+import Cart from "../../../svg/cart";
+import styles from "../header.module.scss";
 
-export default function HeaderMiddle({ setShowModal, showModal }) {
+export default function HeaderMiddle({ setShowModal, showModal, width }) {
+  const { cartTotalQuantity } = useSelector((state) => state.cart);
+
   return (
     <div className={styles.header_middle}>
-    <div className={styles.header_middle_logo}>
-      <Link href="/">
-        <span>WATCH-STORE</span>
-      </Link>
-    </div>
-    <div className={styles.header_middle_contacts}>
-      <div className={styles.header_middle_contacts_top}>
-        <span>+38(067) 555-66-77</span>
-        <button onClick={() => setShowModal(!showModal)}>
-          <i
-            className="bi bi-arrow-down-short"
-            style={{ fontSize: "1rem", color: "red" }}
-          ></i>
-        </button>
-        <div
-          className={
-            showModal
-              ? styles.header_middle_contacts_top_modal
-              : styles.header_middle_contacts_top_modal_hidden
-          }
-        >
-         <HeaderModal setShowModal={setShowModal}/>
+      <div className={styles.header_middle_logo}>
+        <Link href="/">
+          <span>WATCH-STORE</span>
+        </Link>
+      </div>
+      <div className={styles.middle_contacts}>
+        <div className={styles.header_middle_contacts_top}>
+          <span>+38(067) 555-66-77</span>
+          <button onClick={() => setShowModal(!showModal)}>
+            <i
+              className="bi bi-arrow-down-short"
+              style={{ fontSize: "1rem", color: "red" }}
+            ></i>
+          </button>
+          <div
+            className={
+              showModal
+                ? styles.header_middle_contacts_top_modal
+                : styles.header_middle_contacts_top_modal_hidden
+            }
+          >
+            <HeaderModal setShowModal={setShowModal} />
+          </div>
+        </div>
+        <div className={styles.header_middle_contacts_bottom}>
+          <button>Замовити зворотній дзвінок </button>
+          <form></form>
+          <button>Контакти</button>
         </div>
       </div>
-      <div className={styles.header_middle_contacts_bottom}>
-        <button>Замовити зворотній дзвінок </button>
-        <form></form>
-        <button>Контакти</button>
-      </div>
+
+      <HeaderSearchForm />
+
+      {width > 992 && (
+        <div className={styles.header_middle_icons}>
+          <Link href="/wishlist">
+            <div className={styles.header_middle_icons_wishlist}>
+              <Heart />
+              <span>0</span>
+            </div>
+          </Link>
+
+          <i className="bi bi-person-circle"></i>
+          <Link href="/cart">
+            <div className={styles.cart}>
+              <Cart />
+              <span>{cartTotalQuantity}</span>
+            </div>
+          </Link>
+        </div>
+      )}
     </div>
-
-    <HeaderSearchForm />
-
-    <div className={styles.header_middle_icons}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="36"
-        height="36"
-        fill="red"
-        class="bi bi-heart-fill"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-        />
-      </svg>
-
-      <i className="bi bi-suit-heart"></i>
-      <i className="bi bi-person-circle"></i>
-      <div className={styles.cart}>
-        <i className="bi bi-bag"></i>
-        <span>0</span>
-      </div>
-    </div>
-  </div>
-  )
+  );
 }
