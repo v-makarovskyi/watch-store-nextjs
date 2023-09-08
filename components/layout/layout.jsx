65b32471dd43aa4fe4ed6e4c05ef6b4066e27be1
Vladimir <bigcorvet@gmail.com>
2023-09-08 20:38:22 +0300

    added useEffects for cart

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,33 +1,40 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTotals, get_cart_items } from "../../redux/cartSlice";
+import { ToastContainer } from "react-toastify";
 import Head from "next/head";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import { Container } from "react-bootstrap";
-import styles from "./layout.module.css";
+import styles from "./layout.module.scss";
 
 export const siteTitle = "watch-store-nextjs";
 
-
 export default function Layout({ children }) {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(get_cart_items());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getTotals());
+  }, [dispatch, cart]);
 
   return (
-    <Container fluid className={styles.layout_container}>
+    <div className={styles.container}>
       <Head>
         <meta
           name="description"
           content="Online watch store. Developed with NextJS, React and PostgreSQL"
         />
         <meta name="og:title" content={siteTitle} />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-          crossOrigin="anonymous"
-        />
       </Head>
-        <Header /> 
-      <main>{children}</main>
-      <Footer/>
-    </Container>
+      <Header />
+      <ToastContainer />
+      <main>{children && children}</main>
+      <Footer />
+    </div>
   );
 }
