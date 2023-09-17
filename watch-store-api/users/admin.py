7942f9067fa105fd04bbdc37741c2326cf5c3f23
Vladimir <bigcorvet@gmail.com>
2023-09-17 18:31:17 +0300

    added admin.py

--- watch-store-api/users/admin.py
+++ watch-store-api/users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import MyUser
 
-# Register your models here.
+class MyUserAdminManager(UserAdmin):
+    model = MyUser
+    
+    list_display = ('id', 'username', 'lastName', 'email', 'is_active', 'is_staff')
+    ordering = ("id",)
+    fieldsets = (
+        (None, {'fields' : ('email', 'username', 'lastName', 'password', 'start_date')}),
+        ('Разрешения', {'fields': ('is_staff', 'is_active',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'lastName', 'email',  'password1', 'password2', 'is_active', 'is_staff')
+        }),
+    )
+
+
+admin.site.register(MyUser, MyUserAdminManager)
