be30c85920ad07a5603cd13e57fd7de63bb1d717
Vladimir <bigcorvet@gmail.com>
2023-09-17 20:13:28 +0300

    added path for login

--- watch-store-api/core/urls.py
+++ watch-store-api/core/urls.py
@@ -18,11 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from users.views import MyTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('watch_store.urls', namespace='watch_store')),
+    path('api/users/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
